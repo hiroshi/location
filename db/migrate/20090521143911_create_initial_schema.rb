566a3be2b98eac2dbeb4df4113339d1f6c4eae38
Hiroshi Saito <hiroshi3110@gmail.com>
2009-05-25 09:12:35 +0900

    Let the migration script use right column size option

--- db/migrate/20090521143911_create_initial_schema.rb
+++ db/migrate/20090521143911_create_initial_schema.rb
@@ -2,8 +2,8 @@ class CreateInitialSchema < ActiveRecord::Migration
   def self.up
     create_table :cities do |t|
       t.string :name
-      t.string :country_code, :length => 2
-      t.string :region_code, :length => 2
+      t.string :country_code, :limit => 2
+      t.string :region_code, :limit => 2
       t.float :longitude
       t.float :latitude
     end
@@ -12,14 +12,14 @@ class CreateInitialSchema < ActiveRecord::Migration
 
     create_table :countries do |t|
       t.string :name
-      t.string :country_code, :length => 2
+      t.string :country_code, :limit => 2
     end
     add_index :countries, :country_code, :unique => true
 
     create_table :regions do |t|
       t.string :name
-      t.string :country_code, :length => 2
-      t.string :region_code, :length => 2
+      t.string :country_code, :limit => 2
+      t.string :region_code, :limit => 2
     end
     add_index :regions, [:country_code, :region_code], :unique => true
   end
